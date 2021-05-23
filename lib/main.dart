e668387c2338dfdd44b892161d332a3cc041ee07
Alisha <alishajaryal@gmail.com>
2021-05-23 17:41:43 +0530

    Future builder

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
+import 'package:LEARNING/parsing_json/json_parsing.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
-import 'UI/home.dart';
 //void main()=> runApp(ScaffoldExample());
-void main()=> runApp(new MaterialApp(
-  home: BizCard(),
-));
+//final ThemeData _appTheme =  _buildAppTheme();
+//ThemeData _buildAppTheme() {...}
+
+//TextTheme _appTextTheme(TextTheme base)  {...}
+void main() => runApp(new MaterialApp(
+      /*...*/
+      //...
+      home: JsonParsingSimple(),
+    ));
 
